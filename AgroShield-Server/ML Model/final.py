f1fd3a2d2c2197c481165eefa31447fdb93e1d35
Nikhil Patil <nikhilpatil222003@gmail.com>
2024-09-19 21:01:49 +0530

    model

--- AgroShield-Server/ML Model/final.py
+++ AgroShield-Server/ML Model/final.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import keras
+import json
 import tensorflow_hub as hub
 import numpy as np
 from PIL import Image
@@ -13,7 +15,9 @@ genai.configure(api_key="AIzaSyBZEzIEI6dlzUVCAQN8jaVxRZxrGXb2YPg")
 app = Flask(__name__)
 
 # Load the model from TensorFlow Hub
-model = hub.load("https://www.kaggle.com/models/rishitdagli/plant-disease/TensorFlow2/plant-disease/1")
+# import tensorflow_hub as hub
+model = keras.layers.TFSMLayer("./plant_2_dis", call_endpoint='serving_default')
+
 print("Model loaded successfully.")
 
 
@@ -37,9 +41,13 @@ def preprocess_image(image_file):
     return tf.convert_to_tensor(np.expand_dims(image_array, axis=0), dtype=tf.float32)
 
 # Download class indices JSON file
-class_indices_url = "https://github.com/Rishit-dagli/Greenathon-Plant-AI/releases/download/v0.1.0/class_indices.json"
-response = requests.get(class_indices_url)
-class_indices = response.json()
+class_indices = ""
+with open("./class_indices.json") as fp:
+    class_indices = json.load(fp)
+print(class_indices)
+# class_indices_url = "./class_indices.json"
+# response = requests.get(class_indices_url)
+# class_indices = response.json()
 
 # Convert the class indices to a mapping of {index: label}
 index_to_label = {int(key): value for key, value in class_indices.items()}
@@ -53,24 +61,22 @@ def predict():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
+    # Preprocess the image
     try:
-        # Preprocess the image
         processed_image = preprocess_image(file)
 
         # Make predictions using the model
         predictions = model(processed_image)
-        predictions = predictions[0]
+        predictions = predictions['dense'][0]
 
         # Get the predicted class index
         pred_ind = tf.argmax(predictions).numpy()
-
+        print(pred_ind)
         # Get the corresponding class label
-        predicted_label = index_to_label[pred_ind]
-
+        predicted_label = class_indices[str(pred_ind)]
         return jsonify({'predicted_label': predicted_label}), 200
-
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': e}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
